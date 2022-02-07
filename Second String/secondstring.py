4581b13feaafbd6449f5456aa6e35500399c655d
Renan Riva <renanriva@hotmail.com>
2022-02-07 19:43:01 +0000

    Second String

--- Second String/secondstring.py
+++ Second String/secondstring.py
@@ -1,8 +1,8 @@
 #A program that print a phrase when every second letter is printed in reverse order.
-print ('This program will print out every second character of your phrase in reverse order ')
+print ('This program will print out every second character of your sentence in reverse order ')
 
 #Asking for the phrase to the user and storing it as string
-phrase = str(input('Please type a phrase '))
+phrase = str(input('Please type a sentence '))
 
 #Setting up the variable to store the reversed phrase and creating the index variable
 reversePhrase= ""
