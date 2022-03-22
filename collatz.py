df5d4d2959c3e56c7894e1ad2391b40c8c0f7507
Renan Riva <renanriva@hotmail.com>
2022-03-22 17:56:23 +0000

    Collatz Program

--- collatz.py
+++ collatz.py
@@ -12,5 +12,5 @@ while number != 1: # Continue to repeat the code while the result is different t
         print ('{:01.0F}'.format(number)) #Formating got from (https://stackoverflow.com/questions/32013276/python-integer-formatting) to match the formating in the exercise example
     else: # and if the number is not even it will run this code
         number = ((number*3)+1)
-        print ('{:01.0F}'.format(number))
+        print ('{:01.0F}'.format(number)) 
 
